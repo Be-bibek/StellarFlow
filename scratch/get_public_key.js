778445367af9644d613c6293b160113b281332db
Bibek Das <bibekdas1055@gmail.com>
2026-07-15 02:19:34 +0530

    chore: Consolidate scratch scripts into scripts folder and remove empty directories

--- scratch/get_public_key.js
+++
@@ -1,8 +0,0 @@
-const { Keypair } = require("@stellar/stellar-sdk");
-
-try {
-  const kp = Keypair.fromSecret("SD4UBD4AES72PZC2O6UVKLW5AWSK2XQYZNCKKY7FRLV4SOCU4ORCXRJ5");
-  console.log("PUBLIC KEY FOR STELLAR_SIGNER_SECRET:", kp.publicKey());
-} catch (e) {
-  console.error(e);
-}
